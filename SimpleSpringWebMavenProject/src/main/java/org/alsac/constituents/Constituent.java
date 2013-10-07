4d783329f260e1ee2986717fc2ac956404640c57
ahmedowian <ahmedowian@yahoo.com>
2013-10-07 16:00:35 -0500

    segregating responsibilities

---
+++ SimpleSpringWebMavenProject/src/main/java/org/alsac/constituents/Constituent.java
@@ -0,0 +1,32 @@
+package org.alsac.constituents;
+
+import java.util.Set;
+
+import org.alsac.constituents.ConstituentType;
+import org.alsac.constituents.EmailAddress;
+import org.alsac.web.PhoneNumber;
+import org.springframework.hateoas.Identifiable;
+
+public class Constituent  implements Identifiable<Long> {
+	private Long id;
+	private ConstituentType type;
+	private String firstName;
+	private String lastName;
+	private String address1;
+	private String address2;
+	private String city;
+	private String state;
+	private String zipcode;
+	private Set<PhoneNumber> phoneNumbers;
+	private Set<EmailAddress> emailAddresses;
+	
+	
+	
+	
+	@Override
+	public Long getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
