5e0bba0741fb553946cc03b1d480400f0fb3bdd6
ahmedowian <ahmedowian@yahoo.com>
2013-10-07 17:29:41 -0500

    moved

---
+++ SimpleSpringWebMavenProject/src/main/java/org/alsac/web/PhoneNumberResource.java
@@ -0,0 +1,8 @@
+package org.alsac.web;
+
+import org.alsac.constituents.PhoneNumber;
+import org.springframework.hateoas.ResourceSupport;
+
+public class PhoneNumberResource extends ResourceSupport {
+	public PhoneNumber phoneNumber;
+}
---
+++ SimpleSpringWebMavenProject/src/main/java/org/alsac/web/PhoneNumbersResource.java
@@ -0,0 +1,9 @@
+package org.alsac.web;
+
+import java.util.*;
+
+import org.springframework.hateoas.ResourceSupport;
+
+public class PhoneNumbersResource extends ResourceSupport {
+	public Set<PhoneNumberResource> phoneNumbers = new HashSet<>();
+}
